cc55695b4232058ad57d993e941f9cf1067fdfda
r-bansal <ramnesh.bansal@gmail.com>
2016-06-15 09:19:47 -0400

    build package via babel to remove client-side webpack warnings

---
+++ dist/event/types.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var EventTypes = {
+  identify: 'identify',
+  page: 'page',
+  track: 'track',
+  alias: 'alias',
+  group: 'group'
+};
+
+exports.default = EventTypes;
